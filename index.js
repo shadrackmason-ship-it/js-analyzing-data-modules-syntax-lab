69761df00a2223a300441ca32e003bf4a52a9650
shadrack mason <shadrack.mason@student.moringaschool.com>
2026-04-15 20:05:58 +0300

    final commit

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-function combineUsers(...args) {
-  const users = args.flat();
+// import datejs (required for formatting dates)
+require('datejs');
 
-  return {
-    users,
-    merge_date: new Date().toISOString().split('T')[0]
+function combineUsers(...args) {
+  // Step 2: initialize return object
+  const combinedObject = {
+    users: []
   };
+
+  // Step 3 & 4: loop and merge arrays
+  args.forEach(arr => {
+    combinedObject.users.push(...arr);
+  });
+
+  // Step 5: add current date in M/d/yyyy format
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  // Step 7: return object
+  return combinedObject;
 }
 
+// export function for testing
 module.exports = combineUsers;
